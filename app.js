c171c63c5c374d1c1353fdc777136b372440b7b2
RuanFigueiredo <ruanthiago67@gmail.com>
2024-02-07 16:41:23 -0300

    jogo em andamento

---
+++ app.js
@@ -0,0 +1,31 @@
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 
+
+function exibirTextoNaTela(tag,texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+
+
+function verificarChute(){
+    let chute = document.querySelector('input').value;
+    
+    if (chute == numeroSecreto){
+        exibirTextoNaTela('h1', 'Acertou! top!');
+        exibirTextoNaTela('p', 'Você descobriu o número com 6 tentativas');
+    }else{
+        if(chute > numeroSecreto){
+            exibirTextoNaTela('p', 'O número secreto é menor');
+        }else{
+            exibirTextoNaTela('p', 'o número secreto é maior');
+        }
+    }
+
+}
+
+function gerarNumeroAleatorio() {
+   return parseInt(Math.random() *10 + 1); 
+}
